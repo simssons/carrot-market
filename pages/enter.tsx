6d1bdf4384dae518554f8cc7d8d2d8caa0838dd5
jaehun <jhsim@kcpglob.com>
2023-01-30 10:58:21 +0900

    New Welcome Msg

--- pages/enter.tsx
+++ pages/enter.tsx
@@ -78,7 +78,7 @@ const Enter: NextPage = () => {
   }, [tokenData, router]);
   return (
     <div className="mt-16 px-4">
-      <h3 className="text-3xl font-bold text-center">Enter to Carrot</h3>
+      <h3 className="text-3xl font-bold text-center">Welcome to my dev</h3>
       <div className="mt-12">
         {data?.ok ? (
           <form
